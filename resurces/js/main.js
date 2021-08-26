2e5677c930299ad0b3096a01b0aacdf6f259ebd3
Musa <smabumusa98gmail.com>
2021-08-26 20:58:37 +0600

    psd to html

---
+++ resurces/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  //waypoints plugin(menu navigation)
+
+
+  // $(".js.services-section").waypoint(function(direction){
+  //   if(direction=="down"){
+  //     $("nav").addClass("sticky"); 
+  //   }else{
+  //     $("nav").removeClass("sticky"); 
+  //   }
+
+  // });
+
+  
+  //mix it up (portfolio section)
+  var mixer = mixitup(".container");
+  //waypoints plugin(menu navigation)
+
+
+});
+
